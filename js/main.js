5fe763732b3b5b5c92abed7ee60e6e5e339f3935
catanielena <catani.elena1@gmail.com>
2021-09-17 17:20:58 +0200

    Add: auto-slider

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ const app = new Vue ({
     el: '#root',
     data: {
         images: ['assets/img/newyork_ortho.png', 'assets/img/newmexico_ortho.png', 'assets/img/newyork_ortho.png', 'assets/img/ncarolina_ortho.png'],
-        imageIndex: 0
+        imageIndex: 0,
+        autoSlider: null
     },
     methods: {
         nextImg: function() {
@@ -13,7 +14,20 @@ const app = new Vue ({
         },
         displayImage: function(i) {
             this.imageIndex = i;
+        },
+        nextImgClick: function() {
+            clearInterval(this.autoSlider);
+            this.nextImg();
+        },
+        prevImgClick: function() {
+            clearInterval(this.autoSlider);
+            this.prevImg();
+        },
+        autoS () {
+            this.autoSlider = setInterval(this.nextImg, 1500);
         }
-
+	},
+    created () {
+        this.autoS()
     }
 });
