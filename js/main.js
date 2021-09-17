3ab33bd32642e2afee3b2be007de2f70339f4258
catanielena <catani.elena1@gmail.com>
2021-09-17 16:19:44 +0200

    Add: slider

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const app = new Vue ({
+    el: '#root',
+    data: {
+        images: ['assets/img/newyork_ortho.png', 'assets/img/newmexico_ortho.png', 'assets/img/newyork_ortho.png', 'assets/img/ncarolina_ortho.png'],
+        imageIndex: 0
+    },
+    methods: {
+        nextImg: function() {
+            return this.imageIndex >= this.images.length - 1 ? this.imageIndex=0 : this.imageIndex++;
+        },
+        prevImg: function() {
+            return this.imageIndex <= 0 ? this.imageIndex=this.images.length - 1 : this.imageIndex--;
+        },
+
+    }
+});
